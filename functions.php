105314fffbb854ff6b795a02ef9de7c4a47ddd99
Inna Kovtun <39884162+InnaKovtun@users.noreply.github.com>
2021-02-15 17:38:57 +0200

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,24 @@
+<?php
+	add_action( 'wp_enqueue_scripts', 'invest_theme_style');
+	add_action( 'wp_enqueue_scripts', 'jquery_lib');
+	add_action( 'wp_footer', 'invest_theme_style_footer');
+
+	function invest_theme_style(){
+		wp_enqueue_style('style', get_stylesheet_uri());
+		wp_enqueue_style('bootstrap', get_template_directory_uri() . '/assets/css/bootstrap-grid.min.css' );
+	}
+
+	function invest_theme_style_footer(){
+		wp_enqueue_style('slick', get_template_directory_uri() . '/assets/slick/slick.css');
+		wp_enqueue_style('slick-theme', get_template_directory_uri() . '/assets/slick/slick-theme.css');
+	}
+
+	function jquery_lib(){
+		wp_deregister_script( 'jquery' );
+		wp_register_script( 'jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js');
+		wp_enqueue_script('jquery');
+		wp_enqueue_script('slick_script', get_template_directory_uri() . '/assets/slick/slick.js', array('jquery'), null, true);
+		wp_enqueue_script('main', get_template_directory_uri() . '/assets/js/main.js', array('jquery'), null, true);	
+	}
+	
+?>
---
+++ index.php
@@ -0,0 +1,201 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>СтройИнвест - корпоративный сайт</title>
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <?php wp_head()?>    
+
+</head>
+<body>
+    
+    <div class="container">
+        <div class="top-nav">
+            <div class="row justify-content-between align-items-center">
+                <div class="col-xl-auto col-sm-3 ">
+                    <div class="logo">
+                        <img src="img/logo.png" alt="logo">
+                    </div>
+                </div>
+                <!-- /.col-md -->
+                <div class="col-md-auto col-sm-9">
+                    <button class="top-nav_btn">
+                        <i class="icon-menu"></i>
+                    </button>
+                    <ul class="top-nav_menu">
+                        <li><a href="#">Главна</a></li>
+                        <li><a href="#">О компании</a></li>
+                        <li><a href="#">Наши услуги</a></li>
+                        <li><a href="#">Структура</a></li>
+                        <li><a href="#">Инвеcторам</a></li>
+                        <li><a href="#">Контакты</a></li>
+                    </ul>
+                </div>
+                <!-- /.col-md -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.top-nav -->
+        <div class="row">
+            <div class="col-md-4">
+                <div class="left-sidebar">
+                    <div class="left-sidebar_btn">
+                        Категории
+                    </div>
+
+                    <ul class="left-sidebar_menu">
+                        <li><a href="#">Земля</a></li>
+                        <li><a href="#">Жилая недвижимость</a></li>
+                        <li><a href="#">Коммерческая недвижимость</a></li>
+                        <li><a href="#">Новостройки</a></li>
+                        <li><a href="#">Дома/Коттеджи</a></li>
+                        <li><a href="#">Строительство</a></li>
+                        <li><a href="#">Аренда</a></li>
+                    </ul>
+
+                    <div class="left-sidebar_object">
+                        <img src="img/objects/object-preview-1.png" alt="object-preview">
+                        <div class="left-sidebar_object-description">
+                            <h3>жилой комплекс в г. Москва</h3>
+                            <p>190-квартирный 12-этажный жилой дом № 88 по ул. Ленина в микрорайоне Центральный...</p>
+                            <a href="#">Узнать подробнее »</a>
+                        </div>
+                    </div>
+
+                    <div class="left-sidebar_object">
+                        <img src="img/objects/object-preview-2.png" alt="object-preview">
+                        <div class="left-sidebar_object-description">
+                            <h3>жилой комплекс в г. Москва</h3>
+                            <p>190-квартирный 12-этажный жилой дом № 88 по ул. Ленина в микрорайоне Центральный...</p>
+                            <a href="#">Узнать подробнее »</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- /.col-md-4 -->
+            <div class="col-md-8">
+                <div class="content">
+                    <h2>СтройИнвест</h2>
+                    <p>Проснувшись однажды утром после беспокойного сна, Грегор Замза обнаружил, что он у себя в постели превратился в страшное насекомое. Лежа на панцирнотвердой спине, он видел, стоило ему приподнять голову, свой коричневый, выпуклый, разделенный дугообразными чешуйками живот, на верхушке которого еле держалось готовое вот-вот окончательно сползти одеяло.</p>
+                    <p>Его многочисленные, убого тонкие по сравнению с остальным телом ножки беспомощно копошились у него перед глазами. «Что со мной случилось?» – подумал он. Это не было сном. Его комната, настоящая, разве что слишком маленькая, но обычная комната, мирно покоилась в своих четырех хорошо знакомых стенах. Над столом, где были разложены распакованные образцы сукон – Замза был коммивояжером, – висел портрет, который он недавно вырезал из иллюстрированного журнала и вставил в красивую золоченую рамку. На портрете была изображена дама в меховой шляпе и боа, она сидела очень прямо и протягивала зрителю тяжелую меховую муфту, в которой целиком исчезала ее рука.</p>
+                    <p>Затем взгляд Грегора устремился в окно, и пасмурная погода – слышно было, как по жести подоконника стучат капли дождя – привела его и вовсе в грустное настроение. «Хорошо бы еще немного поспать и забыть всю эту чепуху», – подумал он, но это было совершенно неосуществимо, он привык спать на правом боку, а в теперешнем своем</p>
+                    <a href="#" class="btn btn-primary">Узнать больше о компании</a>
+                </div>
+                <!-- /.content -->
+                <div class="directions">
+                    <h2>Направления деятельности</h2>
+                    <div class="directions-blocks">
+                        <div class="directions-block">
+                            <div class="directions-block-inner" style="background-image: url(img/ph1.png);">
+                                <h3>Девелоперские проекты</h3>
+                            </div>
+                        </div>
+                        <div class="directions-block">
+                            <div class="directions-block-inner" style="background-image: url(img/ph2.png);">
+                                <h3>Продажа недвижимости</h3>  
+                            </div>
+                        </div>
+                        <div class="directions-block">
+                            <div class="directions-block-inner" style="background-image: url(img/ph3.png);">
+                                <h3>Строительство</h3>   
+                            </div>
+                        </div>
+                        <div class="directions-block">
+                            <div class="directions-block-inner" style="background-image: url(img/ph4.png);">
+                                <h3>Инвестиционные программы</h3> 
+                            </div>
+                        </div>
+                        <div class="directions-block">
+                            <div class="directions-block-inner" style="background-image: url(img/ph1.png);">
+                                <h3>Финансовые инструменты</h3>  
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- /.col-md-8 -->
+        </div>
+        <!-- /.row -->
+
+    </div>
+    <!-- /.container -->
+
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="copy">
+                        <p>Copyright © 2014-2017</p>
+                        <a class="footer-policy-link" href="#">Политика конфиденциальности</a>
+                        <small>Но как можно было спокойно спать под этот сотрясающий мебель трезвон? Ну, спал-то он неспокойно, но, видимо, крепко. Однако что делать теперь? Следующий поезд уходит в семь часов; чтобы поспеть на него, он должен отчаянно торопиться, а набор образцов.</small>
+                    </div>
+                </div>
+                <!-- /.col-md -->
+                <div class="col-md-2 col-6">
+                    <ul class="footer-menu">
+                        <li><a href="#">Главна</a></li>
+                        <li><a href="#">О компании</a></li>
+                        <li><a href="#">Наши услуги</a></li>
+                        <li><a href="#">Структура</a></li>
+                        <li><a href="#">Инвеcторам</a></li>
+                        <li><a href="#">Контакты</a></li>
+                    </ul>
+                </div>
+                <!-- /.col-md -->
+                <div class="col-md col-6">
+                    <ul class="footer_menu">
+                        <li><a href="#">Земля</a></li>
+                        <li><a href="#">Жилая недвижимость</a></li>
+                        <li><a href="#">Коммерческая недвижимость</a></li>
+                        <li><a href="#">Новостройки</a></li>
+                        <li><a href="#">Дома/Коттеджи</a></li>
+                        <li><a href="#">Строительство</a></li>
+                        <li><a href="#">Аренда</a></li>
+                    </ul>
+                </div>
+                <!-- /.col-md -->
+                <div class="col-md">
+                    <div class="footer-contacts">
+                        <p class="footer-contacts_phone">8 (800) 850-50-50</p>
+                        <p class="footer-contacts_address">г. Москва, ул. Морозова, д.12, 3 этаж</p>
+                        <p class="footer-contacts_address">г. Москва, ул. Ленина, д. 23, офис 2</p>
+                        <p class="footer-contacts_email">invest@mail.ru</p>
+
+                    </div>
+                    <div class="footer-social-links">
+                        <ul>
+                            <li><a href="#"><i class="icon-vkontakte"></i></a></li>
+                            <li><a href="#"><i class="icon-facebook"></i></a></li>
+                            <li><a href="#"><i class="icon-instagram"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <!-- /.col-md -->
+            </div>
+        </div>
+        <!-- /.container -->
+    </footer>
+    <div class="modal">
+        <div class="modal-content">
+            <div class="close">&times</div>
+            <h4>Заполните поля</h4>
+            <p>Мы свяжемся с вами в ближайшее время</p>
+            <div class="modal-form">
+                <form action="">
+                    <input type="text" placeholder="Ваше имя">
+                    <input type="text" placeholder="Ваш телефон">
+                    <button type="submit" class="btn btn-primary modal-form_btn">Отправить заявку</button>
+                    <small>Нажима кнопку, даю свое согласие на<br>
+                        <a href="#">обработку персональных данных</a>
+                    </small>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <?php wp_footer()?>
+</body>
+</html>
